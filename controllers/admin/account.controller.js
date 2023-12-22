b5855e17d2ccc17174f184ae1f579e8819dda45c
Your Name <you@example.com>
2023-12-22 23:59:18 +0700

    lam tinh nang dang nhap

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -98,3 +98,4 @@ module.exports.editPatch = async (req, res) => {
     res.redirect(`back`);
   }
 }
+
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -5,10 +5,12 @@ const productRouter = require("./product.route");
 const productCategoryRouter = require("./products-category.route");
 const roleRouter = require("./role.route");
 const accountRouter = require("./account.route");
+const authRouter = require("./auth.route");
 module.exports = (app) => {
   app.use(systemconfig.prefixAdmin + "/dashboard", dashboardRouter);
   app.use(systemconfig.prefixAdmin + "/products", productRouter);
   app.use(systemconfig.prefixAdmin + "/products-category", productCategoryRouter);
   app.use(systemconfig.prefixAdmin + "/roles", roleRouter);
   app.use(systemconfig.prefixAdmin + "/accounts", accountRouter)
+  app.use(systemconfig.prefixAdmin + "/auth", authRouter)
 }
