295519ceb3360bfe2376f96e280cc13f6877a5fc
Your Name <you@example.com>
2023-12-26 10:06:31 +0700

    them author(phan quyen)

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,5 +1,6 @@
 const systemConfig = require("../../config/system");
 const Account = require("../../models/account.model");
+const Role = require("../../models/role.model");
 module.exports.requiredAuth = async (req, res, next) => {
   if(!req.cookies.token) {
     res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
@@ -8,10 +9,16 @@ module.exports.requiredAuth = async (req, res, next) => {
       token: req.cookies.token,
       deleted: false,
       status: "active"
-    });
+    }).select("-password -token -phone");
+    const role = await Role.findOne({
+      _id: user.role_id,
+      deleted: false
+    }).select("title permissions");
     if(!user) {
       res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
     } else {
+      res.locals.user = user;
+      res.locals.role = role;
       next();
     }
   }
